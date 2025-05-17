52f3625343043050dc2ca5705ccaccb9131a68fe
Varenik575 <invisibleredhead5@gmail.com>
2025-05-17 22:11:12 +0300

    working form base without Formik

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
+
+function App() {
+  
+
+  return (
+    <>
+      <ContactForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import styles from "./ContactForm.module.css";
+
+function ContactForm() {
+  const INITIAL_STATE = { name: "", number: "" };
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData(INITIAL_STATE);
+  };
+
+  const handleChangeInput = (event) => {
+    const { name, value } = event.target;
+
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="name">
+          <span className={styles.formTag}>Name</span>
+          <input
+            onChange={handleChangeInput}
+            value={formData.name}
+            className={styles.input}
+            type="text"
+            name="name"
+          />
+        </label>
+        <label htmlFor="number">
+          <span className={styles.formTag}>Number</span>
+          <input
+            onChange={handleChangeInput}
+            value={formData.number}
+            className={styles.input}
+            type="text"
+            name="number"
+          />
+        </label>
+        <button
+          className={styles.button}
+          type="submit"
+        >
+          Add Contact
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
